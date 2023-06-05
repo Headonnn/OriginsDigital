9c6ac0739c4b93ab468cd433414ac245dffb2ed9
chloeafp <chloeafp@gmail.com>
2023-06-05 14:45:16 +0200

    tailwind

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,7 @@
-import Home from "./pages/Home";
-
-import "./App.css";
-
 function App() {
   return (
     <div className="App">
-      <Home />
-      <p>coucou</p>
+      <p className="text-3xl font-bold underline">coucou</p>
     </div>
   );
 }
