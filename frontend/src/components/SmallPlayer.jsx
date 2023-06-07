b0075cdc04090b81f28395b7862da8d8a5af58da
Headonnn <[th.couraud@gmail.com](th.couraud@gmail.com)>
2023-06-07 16:25:27 +0200

    repush

--- frontend/src/components/SmallPlayer.jsx
+++ frontend/src/components/SmallPlayer.jsx
@@ -3,11 +3,16 @@ import ReactPlayer from "react-player";
 import VideoContext from "../../contexts/VideoContext";
 
 function SmallPlayer() {
-  const { currentUrl } = useContext(VideoContext);
+  const { currentVideo } = useContext(VideoContext);
 
   return (
     <div>
-      <ReactPlayer url={currentUrl} controls width="384px" height="216px" />
+      <ReactPlayer
+        url={currentVideo.url}
+        controls
+        width="384px"
+        height="216px"
+      />
     </div>
   );
 }
