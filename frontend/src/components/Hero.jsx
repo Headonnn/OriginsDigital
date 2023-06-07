d23c8eec12ef66aceba334205ebedd5fa819b492
Headonnn <[th.couraud@gmail.com](th.couraud@gmail.com)>
2023-06-07 15:48:56 +0200

    lien entre le back et le front

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -7,42 +7,22 @@ import {
 } from "react-icons/bs";
 import { Link } from "react-router-dom";
 import { RxDotFilled } from "react-icons/rx";
-import img1 from "../assets/images/andhika-soreng-US06QF_sxu8-unsplash.jpg";
-import img2 from "../assets/images/jeremy-bishop-_CFv3bntQlQ-unsplash.jpg";
-import img3 from "../assets/images/michal-parzuchowski-LQsJUtKmPlg-unsplash.jpg";
+
 import VideoContext from "../../contexts/VideoContext";
 
-import skate from "../assets/videos/skate.mp4";
-import surf from "../assets/videos/surf.mp4";
-
 function Hero() {
-  const { setCurrentUrl } = useContext(VideoContext);
-  const test = [
-    {
-      thumbmail: img1,
-      name: "Magnifique video de velo",
-      url: skate,
-    },
-    {
-      thumbmail: img2,
-      name: "Magnifique video de surf",
-      url: surf,
-    },
-    {
-      thumbmail: img3,
-      name: "Magnifique video de petanque",
-      url: skate,
-    },
-  ];
+  const { dataVideo, setCurrentUrl } = useContext(VideoContext);
+
+  if (dataVideo === undefined) return null;
 
   const [index, setIndex] = useState(0);
   const prevSlide = () => {
     const isFirstSlide = index === 0;
-    const newIndex = isFirstSlide ? test.length - 1 : index - 1;
+    const newIndex = isFirstSlide ? dataVideo.length - 1 : index - 1;
     setIndex(newIndex);
   };
   const nextSlide = () => {
-    const isLastSlide = index === test.length - 1;
+    const isLastSlide = index === dataVideo.length - 1;
     const newIndex = isLastSlide ? 0 : index + 1;
     setIndex(newIndex);
   };
@@ -55,56 +35,58 @@ function Hero() {
   }, [index]);
 
   return (
-    <div className=" h-[50vh] w-full m-auto  group ">
-      <div
-        style={{ backgroundImage: `url(${test[index].thumbmail})` }}
-        className="w-full h-full  bg-center bg-cover duration-500 p-[16px]"
-      >
-        <div className="flex flex-col-reverse h-[100%]  ">
-          <div className="flex justify-center ">
-            {test.map((e, i) => (
-              <div
-                role="presentation"
-                key={test[i].url}
-                onClick={() => setIndex(i)}
-                onKeyDown={() => setIndex(i)}
-                className={
-                  index === i
-                    ? "text-white cursor-pointer"
-                    : "text-black/20 cursor-pointer"
-                }
-              >
-                <RxDotFilled />
-              </div>
-            ))}
-          </div>
-          <div className="flex grow items-center justify-between">
-            <div className="  flex-none -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer ">
-              <BsChevronCompactLeft onClick={prevSlide} size={30} />
-            </div>
-            <div className="flex grow justify-center items-end h-[100%] gap-[20%]">
-              <div className=" flex   items-center gap-[16px]  bg-white text-black rounded-xl  mb-[16px] p-[12px] cursor-pointer">
-                <BsFillPlayFill /> Lecture
-              </div>
-              <Link to="/description">
+    dataVideo.length > 0 && (
+      <div className=" h-[50vh] w-full m-auto  group ">
+        <div
+          style={{ backgroundImage: `url(${dataVideo[index].thumbnail})` }}
+          className="w-full h-full  bg-center bg-cover duration-500 p-[16px]"
+        >
+          <div className="flex flex-col-reverse h-[100%]  ">
+            <div className="flex justify-center ">
+              {dataVideo.map((e, i) => (
                 <div
-                  className="    flex items-center gap-[16px] bg-black/50 text-white rounded-xl  mb-[16px] p-[12px] cursor-pointer"
-                  onClick={() => setCurrentUrl(test[index].url)}
-                  onKeyDown={() => setCurrentUrl(test[index].url)}
                   role="presentation"
+                  key={dataVideo[i].url}
+                  onClick={() => setIndex(i)}
+                  onKeyDown={() => setIndex(i)}
+                  className={
+                    index === i
+                      ? "text-white cursor-pointer"
+                      : "text-black/20 cursor-pointer"
+                  }
                 >
-                  <BsInfoCircle />
-                  Plus d'infos
+                  <RxDotFilled />
                 </div>
-              </Link>
+              ))}
             </div>
-            <div className=" flex-none -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer ">
-              <BsChevronCompactRight onClick={nextSlide} size={30} />
+            <div className="flex grow items-center justify-between">
+              <div className="  flex-none -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer ">
+                <BsChevronCompactLeft onClick={prevSlide} size={30} />
+              </div>
+              <div className="flex grow justify-center items-end h-[100%] gap-[20%]">
+                <div className=" flex   items-center gap-[16px]  bg-white text-black rounded-xl  mb-[16px] p-[12px] cursor-pointer">
+                  <BsFillPlayFill /> Lecture
+                </div>
+                <Link to="/description">
+                  <div
+                    className="    flex items-center gap-[16px] bg-black/50 text-white rounded-xl  mb-[16px] p-[12px] cursor-pointer"
+                    onClick={() => setCurrentUrl(dataVideo[index].url)}
+                    onKeyDown={() => setCurrentUrl(dataVideo[index].url)}
+                    role="presentation"
+                  >
+                    <BsInfoCircle />
+                    Plus d'infos
+                  </div>
+                </Link>
+              </div>
+              <div className=" flex-none -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer ">
+                <BsChevronCompactRight onClick={nextSlide} size={30} />
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    )
   );
 }
 
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,16 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
 import NavBar from "../components/NavBar";
 import Hero from "../components/Hero";
 import CarouselAll from "../components/CarouselAll";
 import Footer from "../components/Footer";
+import VideoContext from "../../contexts/VideoContext";
 
 function HomePage() {
+  const { dataVideo } = useContext(VideoContext);
   return (
     <>
       <NavBar />
-      <Hero />
-
-      <CarouselAll />
+      {dataVideo.length > 0 && (
+        <div>
+          <Hero />
+          <CarouselAll />
+        </div>
+      )}
       <Footer />
     </>
   );
