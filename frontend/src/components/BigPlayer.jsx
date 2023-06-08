ad25c19b4d644a62ac1d328266c961f31714012d
Headonnn <[th.couraud@gmail.com](th.couraud@gmail.com)>
2023-06-08 11:02:52 +0200

    page watch

---
+++ frontend/src/components/BigPlayer.jsx
@@ -0,0 +1,28 @@
+import React, { useContext } from "react";
+import ReactPlayer from "react-player";
+
+import { BsArrowLeft } from "react-icons/bs";
+import { Link, useParams } from "react-router-dom";
+import VideoContext from "../../contexts/VideoContext";
+
+function BigPlayer() {
+  const params = useParams();
+  const { dataVideo } = useContext(VideoContext);
+  return (
+    <div className="player-wrapper relative">
+      <Link to={`/description/${params.id}`}>
+        <div className="absolute left-1 top-1 text-white cursor-pointer hover:scale-105 transition">
+          <BsArrowLeft size={30} />
+        </div>
+      </Link>
+      <ReactPlayer
+        url={dataVideo[params.id].url}
+        controls
+        width="100vw"
+        height="100vh"
+      />
+    </div>
+  );
+}
+
+export default BigPlayer;
--- frontend/src/navigation/Router.jsx
+++ frontend/src/navigation/Router.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import HomePage from "../pages/HomePage";
 import Contact from "../pages/Contact";
 import VideoDescription from "../pages/VideoDescription";
+import Watch from "../pages/Watch";
 
 function Router() {
   return (
@@ -10,6 +11,7 @@ function Router() {
       <Route path="/" element={<HomePage />} />
       <Route path="/contact" element={<Contact />} />
       <Route path="/description/:id" element={<VideoDescription />} />
+      <Route path="/watch/:id" element={<Watch />} />
     </Routes>
   );
 }
