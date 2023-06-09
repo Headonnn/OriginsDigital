7e143f084310eb8514e38d2e67c35c9910949d27
JeremyHerpe <herpe.jeremy@gmail.com>
2023-06-09 10:00:44 +0200

    database damagecontrol

--- backend/database.sql
+++ backend/database.sql
@@ -79,31 +79,3 @@ CREATE TABLE `section` (
 	FOREIGN KEY (carousel_category) REFERENCES `carousel_category` (id),
 	FOREIGN KEY (advert) REFERENCES `advert` (id)
 );
-
-INSERT INTO user (username, email, firstname, lastname, password)
-VALUES 
-("Camille", "camille@camille.com", "Camille", "Le Floch", "1234"),
-("Thomas", "thomas@thomas.com", "Thomas", "Couraud", "1234"),
-("Emilie", "emilie@emilie.com", "Emilie", "Nathan", "1234"),
-("Chloe", "chloe@chloe.com", "Chloe", "Peltier", "1234"),
-("Jeremy", "jeremy@jeremy.com", "Jeremy", "Herpe", "1234");
-
-INSERT INTO category (name) 
-VALUES 
-("Surf"),
-("Petanque"),
-("Escalade"),
-("Ski");
-
-INSERT INTO video (title, url, description, thumbnail,date)
-VALUES
-("Video de skate", "https://www.youtube.com/watch?v=t_9i6vCOwtE&ab_channel=Lucas-WCS", "uUn mec qui fait du ski", "https://cdn.pixabay.com/photo/2014/10/22/08/38/skateboard-497706_1280.jpg","2023-06-06"),
-("Video de surf", "https://www.youtube.com/watch?v=gfxAZIX8hC4&list=PLDzslQtErjzjJs4Y5Te7kVoA3OwXGT2Td&ab_channel=BlitzstreamFacile/", "Un mec qui fait du surf", "https://cdn.pixabay.com/photo/2020/08/04/13/28/man-5462849_1280.jpg","2023-04-06"),
-("Video d'escalade", "https://www.youtube.com/watch?v=JgWU5Ou6SPU&ab_channel=EurosportFrance", "Un mec qui fait de la grimpette", "https://cdn.pixabay.com/photo/2013/02/10/00/02/bear-79838_1280.jpg","2015-08-06"),
-("Video de pétanque", "https://www.youtube.com/watch?v=v9ft_84aOjw&ab_channel=MeilleursMomentsdeP%C3%A9tanque", "des gens qui jouent à la pétanque", "https://cdn.pixabay.com/photo/2015/09/13/05/05/bmx-937725_1280.jpg","2005-08-06");
-
---   ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-
-
-
