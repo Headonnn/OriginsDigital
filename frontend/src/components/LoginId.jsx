e7ab5b92323d5e6ccab0d90a139e65fad8011472
JeremyHerpe <herpe.jeremy@gmail.com>
2023-07-17 10:28:11 +0200

    créé message d'erreur login

--- frontend/src/components/LoginId.jsx
+++ frontend/src/components/LoginId.jsx
@@ -15,6 +15,13 @@ function LoginId() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  const [loginError, setLoginError] = useState(false);
+  const resetPage = () => {
+    setLoginError(false);
+    navigate("/login");
+  };
+
+
   const { setDataLogin } = useContext(LoginContext);
 
   const handleLogin = (e) => {
@@ -44,75 +51,102 @@ function LoginId() {
       })
       .catch((error) => {
         console.error("Catch level, Error during login attempt", error);
+        setLoginError(!loginError);
       });
   };
 
   return (
-    <div className="loginid-container bg-black p-5 py-8 relative overflow-hidden">
-      <div className="bg-gradient-to-br from-blue-900 relative flex flex-col items-center px-10 py-16 mx-auto sm:max-w-md my-10 shadow-[inset0-2px_4px_rgba(0,0,0,0.6)] text-white rounded-[31px]">
-        <p className="text-white text-xl text-center mt-15">
-          Se connecter à Mon Compte
-        </p>
-        <div className="rounded-lg p-4 mt-16">
-          <form onSubmit={handleLogin}>
-            <div className="relative">
-              <input
-                type="email"
-                className=" text-blue-800 placeholder-blue-800 font-regular text-md rounded-md px-3 py-3 mb-4 border-black  w-full md:w-96 pr-10"
-                placeholder="votre adresse mail"
-                onChange={(e) => setEmail(e.target.value)}
-                value={email}
-              />
-              <BsCheckCircle className="absolute top-1/2 right-2 transform -translate-y-1/2 text-green-500 w-6 h-6" />
-            </div>
-            <div className="relative">
-              <input
-                type={passwordVisible ? "text" : "password"}
-                className="text-blue-800 placeholder-blue-800 text-md rounded-md px-3 py-3 border border-black w-full md:w-106 pr-10"
-                placeholder="votre mot de passe"
-                onChange={(e) => setPassword(e.target.value)}
-                value={password}
-              />
-              {passwordVisible ? (
-                <IoEyeSharp
-                  className="absolute top-1/2 right-2 transform -translate-y-1/2 text-blue-800 w-6 h-6 cursor-pointer"
-                  onClick={togglePasswordVisibility}
-                />
-              ) : (
-                <IoEyeOffSharp
-                  className="absolute top-1/2 right-2 transform -translate-y-1/2 text-blue-800 w-6 h-6 cursor-pointer"
-                  onClick={togglePasswordVisibility}
-                />
-              )}
-            </div>
-            <p className="text-white text-sm font-light font-poppins mt-2">
-              Mot de passe oublié ? Cliquez{" "}
-              <span className="underline font-bold duration-200 hover:text-orange-600">
-                ici
-              </span>
-              .
-            </p>
-            <div className="flex flex-col space-y-2 mt-16 w-full">
-              <button
-                type="submit"
-                className="bg-gradient-to-r from-red-600 to-orange-500 text-white placeholder-white text-md rounded-md px-3 py-3 border border-black  w-full md:w-106 pr-10"
-              >
-                S'identifier
-              </button>
+    <>
+      {loginError ? (
 
-              <p className="mt-4 text-sm text-center">
-                Première visite sur Origins Digital ?{" "}
-                <NavLink to="/createaccountform">
-                  <span className="font-bold duration-200 hover:text-orange-600">
-                    Inscrivez-vous
-                  </span>
-                </NavLink>
-              </p>
+        <div className="h-[60vh] flex items-center justify-center">
+          <div className="bg-gradient-to-br from-blue-900 flex flex-col items-center justify-center py-16 px-8 max-w-md text-white rounded-[31px]">
+            <p className="text-white pt-8 pb-16 text-lg md:text-2xl ">
+              Vos identifiants sont incorrects
+            </p>
+            <div className="flex justify-center">
+              <NavLink to="/login">
+                <button
+                  type="button"
+                  onClick={resetPage}
+                  className="border hover:bg-white tracking-wide hover:text-black rounded-xl py-2 px-3 text-sm md:px-6  md:text-lg transition"
+                >
+                  Retour
+                </button>
+              </NavLink>
             </div>
-          </form>
+          </div>
+        </div >
+
+      ) : (
+        <div className="loginid-container bg-black p-5 py-8 relative overflow-hidden">
+          <div className="bg-gradient-to-br from-blue-900 relative flex flex-col items-center px-10 py-16 mx-auto sm:max-w-md my-10 shadow-[inset0-2px_4px_rgba(0,0,0,0.6)] text-white rounded-[31px]">
+            <p className="text-white text-xl text-center mt-15">
+              Se connecter à Mon Compte
+            </p>
+            <div className="rounded-lg p-4 mt-16">
+              <form onSubmit={handleLogin}>
+                <div className="relative">
+                  <input
+                    type="email"
+                    className=" text-blue-800 placeholder-blue-800 font-regular text-md rounded-md px-3 py-3 mb-4 border-black  w-full md:w-96 pr-10"
+                    placeholder="votre adresse mail"
+                    onChange={(e) => setEmail(e.target.value)}
+                    value={email}
+                  />
+                  <BsCheckCircle className="absolute top-1/2 right-2 transform -translate-y-1/2 text-green-500 w-6 h-6" />
+                </div>
+                <div className="relative">
+                  <input
+                    type={passwordVisible ? "text" : "password"}
+                    className="text-blue-800 placeholder-blue-800 text-md rounded-md px-3 py-3 border border-black w-full md:w-106 pr-10"
+                    placeholder="votre mot de passe"
+                    onChange={(e) => setPassword(e.target.value)}
+                    value={password}
+                  />
+                  {passwordVisible ? (
+                    <IoEyeSharp
+                      className="absolute top-1/2 right-2 transform -translate-y-1/2 text-blue-800 w-6 h-6 cursor-pointer"
+                      onClick={togglePasswordVisibility}
+                    />
+                  ) : (
+                    <IoEyeOffSharp
+                      className="absolute top-1/2 right-2 transform -translate-y-1/2 text-blue-800 w-6 h-6 cursor-pointer"
+                      onClick={togglePasswordVisibility}
+                    />
+                  )}
+                </div>
+                <p className="text-white text-sm font-light font-poppins mt-2">
+                  Mot de passe oublié ? Cliquez{" "}
+                  <span className="underline font-bold duration-200 hover:text-orange-600">
+                    ici
+                  </span>
+                  .
+                </p>
+                <div className="flex flex-col space-y-2 mt-16 w-full">
+                  <button
+                    type="submit"
+                    className="bg-gradient-to-r from-red-600 to-orange-500 text-white placeholder-white text-md rounded-md px-3 py-3 border border-black  w-full md:w-106 pr-10"
+                  >
+                    S'identifier
+                  </button>
+
+                  <p className="mt-4 text-sm text-center">
+                    Première visite sur Origins Digital ?{" "}
+                    <NavLink to="/createaccountform">
+                      <span className="font-bold duration-200 hover:text-orange-600">
+                        Inscrivez-vous
+                      </span>
+                    </NavLink>
+                  </p>
+                </div>
+              </form>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      )
+      };
+    </>
   );
 }
 
